c48abd1a8b4ba4990f1515d1a9f14a63dcfd26d9
evildre <musaew97@gmail.com>
2023-07-03 18:43:54 +0300

    Added Parent Selector

--- Code/Facade/GeneticAlgorithm/AbstractParentsSelector/TournamentSelector.py
+++ Code/Facade/GeneticAlgorithm/AbstractParentsSelector/TournamentSelector.py
@@ -2,13 +2,27 @@ from AbstractParentsSelector import *
 
 
 class TournamentSelector(AbstractParentsSelector):
+    '''
+    TournamentSelector - класс, наследующийся от AbstractParentsSelector.
+    Он реализует метод make_parents() для выбора родителей из популяции с помощью стратегии турнирного отбора.
+    '''
+
     def make_parents(self):
+        # Получаем длину популяции
         population_len = len(self._population)
+
+        # Проходимся по популяции
         for i in range(population_len):
+            # Выбираем три случайных индивида из популяции
             individuals = [randint(0, population_len-1), randint(
                 0, population_len-1), randint(0, population_len-1)]
-            individuals = [(self._population[j], j)for j in individuals]
+
+            # Создаем список кортежей с индивидами и их соответствующими индексами
+            individuals = [(self._population[j], j) for j in individuals]
+
+            # Сортируем индивидов по их приспособленности (предполагая, что они могут быть отсортированы)
             individuals.sort()
+
+            # Добавляем индивида с наивысшей приспособленностью (индекс 0) в список выбранных родителей
             self._selected_parents.append(
                 self._population[individuals[0][1]])
-
