cf87bab92a9f17a08c6071f49c4865c991b3b303
pddemchuk <90906628+pddemchuk@users.noreply.github.com>
2023-07-03 19:02:55 +0300

    added page3

---
+++ Code/Facade/GUI/filelbl.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton
+
+
+class FileLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.setAlignment(qtc.Qt.AlignCenter)
+        self.setText("Перетащите файл")
+        self.setAcceptDrops(True)
+        self.pathToFile = ''
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            for url in event.mimeData().urls():
+                self.pathToFile = url.path()
+            event.acceptProposedAction()
+        else:
+            event.ignore()
+
+
+class SecondWindow(QtWidgets.QMainWindow):
+    submitClicked = qtc.pyqtSignal(str)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("FILES")
+        self.setGeometry(300, 300, 900, 900)
+
+        self.centralWidget = QWidget(self)
+        self.setCentralWidget(self.centralWidget)
+        self.vbox = QtWidgets.QGridLayout(self.centralWidget)
+
+        self.btnNext = QPushButton("далее", self)
+        self.btnNext.setFixedSize(400, 80)
+        self.btnNext.clicked.connect(self.sendSignal)
+
+        self.fileGetter = FileLabel()
+        self.vbox.addWidget(self.fileGetter)
+        self.vbox.addWidget(self.btnNext)
+
+    def sendSignal(self):
+        self.submitClicked.emit(self.fileGetter.pathToFile)
+        self.close()
--- Code/Facade/GUI/qt.py
+++ Code/Facade/GUI/qt.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QWidget, QLabel, QRadioButton, QPushButton, QApplication, QVBoxLayout, QButtonGroup
+from filelbl import SecondWindow
 import sys
 
 
@@ -8,88 +9,96 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("Задача о рюкзаке")
         self.setGeometry(300, 300, 1200, 900)
-        self.box = QVBoxLayout(self)
+        self.vbox = QVBoxLayout(self)
+        self.setLayout(self.vbox)
         self.listOfWeights, self.listOfCosts = [], []
-        self.page1()
+        self.pathToFile = ''
+        self.needOfDataGen = False
+        self.defaultParams = False
+        #page1 = Page1.Page1(self)
+        self.page1withRadioButtons()
 
-    def page1(self):
+    def page1withRadioButtons(self):
         self.group1, self.group2, self.group3 = QButtonGroup(self), QButtonGroup(self), QButtonGroup(self)
 
         self.mainText1 = QLabel("Способ задания входных данных:", self)
 
-        self.inputType1 = QRadioButton("через GUI", self)
-        self.inputType2 = QRadioButton("чтение из файла", self)
-        self.inputType3 = QRadioButton("генерация случайных данных", self)
+        self.rdbtn1 = QRadioButton("через GUI", self)
+        self.rdbtn2 = QRadioButton("чтение из файла", self)
+        self.rdbtn3 = QRadioButton("генерация случайных данных", self)
 
         self.mainText2 = QLabel("Параметры алгоритма:", self)
 
-        self.inputType4 = QRadioButton("задать самостоятельно", self)
-        self.inputType5 = QRadioButton("значения по умолчанию", self)
+        self.rdbtn4 = QRadioButton("задать самостоятельно", self)
+        self.rdbtn5 = QRadioButton("значения по умолчанию", self)
 
         self.mainText3 = QLabel("Визуализация поиска решения:", self)
 
-        self.inputType6 = QRadioButton("пошаговая", self)
-        self.inputType7 = QRadioButton("сразу перейти к решению", self)
+        self.rdbtn6 = QRadioButton("пошаговая", self)
+        self.rdbtn7 = QRadioButton("сразу перейти к решению", self)
 
-        inputTypes = [self.inputType1, self.inputType2, self.inputType3, self.inputType4, self.inputType5,
-                      self.inputType6, self.inputType7]
+        self.radioButtons = [self.rdbtn1, self.rdbtn2, self.rdbtn3, self.rdbtn4, self.rdbtn5,
+                        self.rdbtn6, self.rdbtn7]
         mainTexts = [self.mainText1, self.mainText2, self.mainText2, self.mainText3]
-        for item in inputTypes:
+        for item in self.radioButtons:
             item.setStyleSheet("font: 25 12pt \"Umpush\";")
         for item in mainTexts:
             item.setStyleSheet("font: oblique 14pt \"Umpush\";")
 
-        self.btn = QPushButton("далее", self)
-        self.btn.setFixedSize(400, 80)
-        self.btn.clicked.connect(self.page2)
+        self.btnNext = QPushButton("далее", self)
+        self.btnNext.setFixedSize(400, 80)
+        self.btnNext.clicked.connect(self.choseTypeOfInput)
 
-        self.group1.addButton(self.inputType1)
-        self.group1.addButton(self.inputType2)
-        self.group1.addButton(self.inputType3)
-        self.group2.addButton(self.inputType4)
-        self.group2.addButton(self.inputType5)
-        self.group3.addButton(self.inputType6)
-        self.group3.addButton(self.inputType7)
+        for i in range(2):
+            self.group1.addButton(self.radioButtons[i])
+            self.group2.addButton(self.radioButtons[i+3])
+            self.group3.addButton(self.radioButtons[i+5])
+        self.group1.addButton(self.radioButtons[2])
 
         self.group1.buttonClicked.connect(self.group1Response)
         self.group2.buttonClicked.connect(self.group2Response)
         self.group3.buttonClicked.connect(self.group3Response)
 
-        self.box.addWidget(self.mainText1)
-        self.box.addWidget(self.inputType1)
-        self.box.addWidget(self.inputType2)
-        self.box.addWidget(self.inputType3)
-        self.box.addWidget(self.mainText2)
-        self.box.addWidget(self.inputType4)
-        self.box.addWidget(self.inputType5)
-        self.box.addWidget(self.mainText3)
-        self.box.addWidget(self.inputType6)
-        self.box.addWidget(self.inputType7)
-        self.box.addWidget(self.btn)
-        self.setLayout(self.box)
+        self.vbox.addWidget(self.mainText1)
+        self.vbox.addWidget(self.rdbtn1)
+        self.vbox.addWidget(self.rdbtn2)
+        self.vbox.addWidget(self.rdbtn3)
+        self.vbox.addWidget(self.mainText2)
+        self.vbox.addWidget(self.rdbtn4)
+        self.vbox.addWidget(self.rdbtn5)
+        self.vbox.addWidget(self.mainText3)
+        self.vbox.addWidget(self.rdbtn6)
+        self.vbox.addWidget(self.rdbtn7)
+        self.vbox.addWidget(self.btnNext)
 
     def group1Response(self, btn):
         self.group1res = btn.text()
-        print(self.group1res)
 
     def group2Response(self, btn):
         self.group2res = btn.text()
-        print(self.group2res)
 
     def group3Response(self, btn):
         self.group3res = btn.text()
-        print(self.group3res)
 
-    def page2(self):
-        for i in reversed(range(self.box.count())):
-            self.box.itemAt(i).widget().close()
-            self.box.takeAt(i)
+    def choseTypeOfInput(self):
+        if self.group1res == "через GUI":
+            self.page2withDataInput()
+        elif self.group1res == "чтение из файла":
+            self.page2withFileInput()
+        elif self.group1res == "генерация случайных данных":
+            self.needOfDataGen = True
+            self.choseTypeOfParam()
+
+    def page2withDataInput(self):
+        for i in reversed(range(self.vbox.count())):
+            self.vbox.itemAt(i).widget().close()
+            self.vbox.takeAt(i)
 
         self.mainText1 = QLabel("Входные данные: количество предметов, ограничение на суммарный вес, "
                                 "вес и стоимость предметов.", self)
         self.mainText1.setStyleSheet("font: oblique 14pt \"Umpush\";")
 
-        self.mainText2 = QLabel("Введите количество предметов:", self)
+        self.mainText2 = QLabel("Введите ограничение на суммарный вес:", self)
         self.mainText2.setStyleSheet("font: oblique 13pt \"Umpush\";")
 
         self.page2lvl = 1
@@ -97,34 +106,106 @@ class Window(QWidget):
         self.inputData.setFixedSize(600, 80)
         self.inputData.returnPressed.connect(self.page2_mod, self.page2lvl)
 
-        self.btn = QPushButton("далее", self)
-        self.btn.setFixedSize(400, 80)
-        self.btn.clicked.connect(self.page3)
+        self.btnNext = QPushButton("далее", self)
+        self.btnNext.setFixedSize(400, 80)
+        self.btnNext.clicked.connect(self.choseTypeOfParam)
 
-        self.box.addWidget(self.mainText1)
-        self.box.addWidget(self.mainText2)
-        self.box.addWidget(self.inputData)
-        self.box.addWidget(self.btn)
+        self.vbox.addWidget(self.mainText1)
+        self.vbox.addWidget(self.mainText2)
+        self.vbox.addWidget(self.inputData)
+        self.vbox.addWidget(self.btnNext)
 
     def page2_mod(self):
-        print(self.inputData.text())
         if self.page2lvl == 1:
-            self.numOfItems = int(self.inputData.text())
-            self.mainText2.setText("Введите ограничение на суммарный вес:")
-            self.page2lvl = 2
+            if self.inputData.text().isdigit():
+                self.weightLimit = int(self.inputData.text())
+                self.mainText2.setText("Введите вес и стоимость предмета через пробел:")
+                self.btnForCancelStep = QPushButton("отменить ввод последнего предмета", self)
+                self.btnForCancelStep.setFixedSize(600, 80)
+                self.btnForCancelStep.clicked.connect(self.deleteLastItem)
+                self.vbox.insertWidget(3, self.btnForCancelStep)
+                self.page2lvl = 2
+            else:
+                self.errorMes()
         elif self.page2lvl == 2:
-            self.weightLimit = int(self.inputData.text())
-            self.mainText2.setText("Введите вес и стоимость предмета через пробел:")
-            self.page2lvl = 3
-        elif self.page2lvl == 3:
-            x, y = [int(i) for i in self.inputData.text().split()]
-            self.listOfWeights.append(x)
-            self.listOfCosts.append(y)
+            try:
+                x, y = [int(i) for i in self.inputData.text().split()]
+                self.listOfWeights.append(x)
+                self.listOfCosts.append(y)
+            except ValueError:
+                self.errorMes()
         self.inputData.clear()
+        print(self.listOfWeights, self.listOfCosts)
 
-    def page3(self):
-        self.mainText1.setText("Параметры алгоритма: вероятность кроссинговера, вероятность мутации, размер популяции.")
-        self.mainText2.setText("Введите вероятность кроссинговера (может принимать значения от 0,6 до 0,95):")
+    def deleteLastItem(self):
+        self.listOfCosts.pop()
+        self.listOfWeights.pop()
+
+    def choseTypeOfParam(self):
+        if self.group2res == "задать самостоятельно":
+            self.page3withParamsInput()
+        if self.group2res == "значения по умолчанию":
+            self.choseTypeOfVisual()
+
+    def errorMes(self):
+        msg = QtWidgets.QMessageBox()
+        msg.setStyleSheet("color: rgb(244, 12, 12); font: 75 13pt \"Umpush\";")
+        msg.setWindowTitle("неполадки...")
+        msg.setText("Неверный формат данных!")
+        msg.exec_()
+
+    def page2withFileInput(self):
+        self.file_window = SecondWindow(self)
+        self.file_window.submitClicked.connect(self.getFilePath)
+        self.file_window.show()
+
+    def getFilePath(self, url):
+        self.pathToFile = url
+        print(self.pathToFile)
+        self.choseTypeOfParam()
+
+    def page3withParamsInput(self):
+        for i in reversed(range(self.vbox.count())):
+            self.vbox.itemAt(i).widget().close()
+            self.vbox.takeAt(i)
+
+        self.mainText1 = QLabel("Параметры алгоритма: вероятность кроссинговера, вероятность мутации, размер популяции.",
+                                self)
+        self.mainText1.setStyleSheet("font: oblique 14pt \"Umpush\";")
+
+        self.mainText2 = QLabel("Введите вероятность кроссинговера (может принимать значения от 60% до 95%):", self)
+        self.mainText2.setStyleSheet("font: oblique 13pt \"Umpush\";")
+
+        self.spinParam = QtWidgets.QSpinBox(self, value = 80, maximum = 95, minimum = 60, singleStep = 5, suffix = "%")
+        self.vbox.insertWidget(2, self.spinParam)
+
+        self.btnNext = QPushButton("далее", self)
+        self.btnNext.setFixedSize(400, 80)
+        self.btnNext.clicked.connect(self.page3_mod)
+
+        self.vbox.addWidget(self.mainText1)
+        self.vbox.addWidget(self.mainText2)
+        self.vbox.addWidget(self.spinParam)
+        self.vbox.addWidget(self.btnNext)
+
+    def page3_mod(self):
+        self.crossoverProbability = self.spinParam.value()
+        self.mainText2.setText("Введите размер популяции (может принимать значения от 20 до 100):")
+
+        self.spinParam.setSuffix("")
+        self.spinParam.setMinimum(20)
+        self.spinParam.setMaximum(100)
+        self.spinParam.setValue(30)
+
+        self.btnNext.clicked.disconnect()
+        self.btnNext.clicked.connect(self.choseTypeOfVisual)
+
+    def choseTypeOfVisual(self):
+        self.countOfPopulation = self.spinParam.value()
+        #if self.group3res == "пошаговая"
+
+    def preparingDataForAlg(self):
+        pass
 
 
 def application():
