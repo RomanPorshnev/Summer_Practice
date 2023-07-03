bad0bde10e770b67d8237c05ef1b61c30125cb9c
evildre <musaew97@gmail.com>
2023-07-04 00:13:26 +0300

    AddedMutator

---
+++ Code/Facade/GeneticAlgorithm/AbstractMutator/AbstractMutator.py
@@ -0,0 +1,66 @@
+from typing import Protocol  
+from random import randint  
+import abc  
+
+
+class AbstractMutator(Protocol):
+    '''
+    AbstractMutator - протокол (интерфейс) для мутаторов.
+    '''
+
+    _children_list: list  # Список потомков
+    _selected_population: list  # Список выбранной популяции
+    _probabilities_mutation: list  # Список вероятностей мутации
+
+    def __init__(self, children_list: list, changing_probability: int = None):
+        '''
+        Инициализация экземпляра класса AbstractMutator.
+
+        Args:
+            children_list (list): Список потомков.
+            changing_probability (int, optional): Вероятность мутации. По умолчанию None.
+        '''
+
+        self._children_list = children_list
+        self._selected_population = []
+        self._probabilities_mutation = []
+
+        if changing_probability is None:
+            d = 0.2
+            M = 0.9
+
+            # Вычисление вероятности мутации для каждого потомка
+            for i in range(len(self._children_list)):
+                child, parent1, parent2 = self._children_list[i][0], self._children_list[i][1], self._children_list[i][2]
+                dist = 0
+
+                # Вычисление расстояния между родителями
+                for j in range(len(parent1)):
+                    dist += abs(int(parent1[j]) - int(parent2[j]))**d
+                dist /= len(parent1)
+
+                probability = (1 - dist) * M
+                self._probabilities_mutation.append(probability)
+        else:
+            # Задание одинаковой вероятности мутации для всех потомков, если пользователь задал
+            self._probabilities_mutation = [changing_probability] * len(self._children_list)
+
+    @abc.abstractmethod
+    def make_population(self) -> list:
+        '''
+        Абстрактный метод make_population.
+        Должен быть реализован в дочерних классах для создания популяции.
+
+        Returns:
+            list: Список популяции.
+        '''
+        ...
+
+    def get_selected_children(self):
+        '''
+        Возвращает выбранную популяцию.
+
+        Returns:
+            list: Список выбранной популяции.
+        '''
+        return self._selected_population
---
+++ Code/Facade/GeneticAlgorithm/AbstractMutator/ChangingMutator.py
@@ -0,0 +1,30 @@
+from AbstractMutator import *
+
+
+class ChangingMutator(AbstractMutator):
+    '''
+    ChangingMutator - класс, наследующийся от AbstractMutator.
+    Он реализует метод make_population() для создания популяции с помощью изменяющейся мутации.
+    '''
+
+    def make_population(self) -> list:
+        '''
+        Метод make_population() создает популяцию с использованием изменяющейся мутации.
+
+        Returns:
+            list: Список популяции.
+        '''
+
+        # Проходимся по каждому потомку
+        for i in range(len(self._children_list)):
+            child = self._children_list[i][0]  # Получаем потомка
+            # Проходимся по каждой позиции потомка
+            for j in range(len(child)):
+                # С вероятностью, определенной _probabilities_mutation[i], меняем значение гена на противоположное
+                if randint(0, 100) <= self._probabilities_mutation[i] * 100:
+                    child[j] = '0' * (child[j] == '1') + '1' * (child[j] == '0')
+
+            # Добавляем потомка в список выбранной популяции
+            self._selected_population.append(child)
+
+        return self._selected_population  # Возвращаем список выбранной популяции
