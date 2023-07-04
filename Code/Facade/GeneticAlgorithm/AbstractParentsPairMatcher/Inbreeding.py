2b138b2898a74da223b28d1fca48018f12eafff2
RomanPorshnev <90764887+RomanPorshnev@users.noreply.github.com>
2023-07-04 17:37:28 +0300

    fixed Outbreeding and added Inbreeding

--- Code/Facade/GeneticAlgorithm/AbstractParentsPairMatcher/Inbreeding.py
+++ Code/Facade/GeneticAlgorithm/AbstractParentsPairMatcher/Inbreeding.py
@@ -2,9 +2,9 @@ from AbstractParentsMatcher import *
 import random
 
 
-class Outbreeding(AbstractParentsMatcher):
+class Inbreeding(AbstractParentsMatcher):
     '''
-    Данный метод формирует и возвращает пары родителей, сформированные по принципу аутбридинга
+    Данный метод формирует и возвращает пары родителей, сформированные по принципу инбридинга
     Выходные данные: список кортежей (родитель1, родитель2)
     '''
 
@@ -12,12 +12,12 @@ class Outbreeding(AbstractParentsMatcher):
         while len(self._parents) != 1:
             index_of_first_parent = random.randint(0, len(self._parents) - 1)
             first_parent = self._parents[index_of_first_parent]
-            max_hamming_distance = 0
+            min_hamming_distance = len(first_parent)
             index_of_second_parent = 0
             for i in range(len(self._parents)):
                 hamming_distance = self._hamming_distance(first_parent, self._parents[i])
-                if hamming_distance >= max_hamming_distance and i != index_of_first_parent:
-                    max_hamming_distance = hamming_distance
+                if hamming_distance <= min_hamming_distance and i != index_of_first_parent:
+                    min_hamming_distance = hamming_distance
                     index_of_second_parent = i
             self._parents_pairs.append((first_parent, self._parents[index_of_second_parent]))
             self._parents.pop(index_of_first_parent)
@@ -26,5 +26,5 @@ class Outbreeding(AbstractParentsMatcher):
 
 if __name__ == "__main__":
     arr = [['0', '0', '0'], ['1', '1', '1'], ['0', '0', '1']]
-    out_breeding = Outbreeding(arr)
+    out_breeding = Inbreeding(arr)
     print(out_breeding.make_parents_pairs())
