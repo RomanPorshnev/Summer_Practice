c490233179c6a93a70bc56f73c36e1e2f1334a30
evildre <90715546+evildre@users.noreply.github.com>
2023-07-05 23:34:59 +0300

    Update ChangingMutator.py

--- Code/Facade/GeneticAlgorithm/AbstractMutator/ChangingMutator.py
+++ Code/Facade/GeneticAlgorithm/AbstractMutator/ChangingMutator.py
@@ -17,14 +17,31 @@ class ChangingMutator(AbstractMutator):
 
         # Проходимся по каждому потомку
         for i in range(len(self._children_list)):
-            child = self._children_list[i][0]  # Получаем потомка
+            child1 = self._children_list[i][0]
+            child2 = self._children_list[i][1]  # Получаем потомков
             # Проходимся по каждой позиции потомка
-            for j in range(len(child)):
+            for j in range(len(child1)):
                 # С вероятностью, определенной _probabilities_mutation[i], меняем значение гена на противоположное
                 if randint(0, 100) <= self._probabilities_mutation[i] * 100:
-                    child[j] = '0' * (child[j] == '1') + '1' * (child[j] == '0')
-
+                    child1[j] = '0' * (child1[j] == '1') + \
+                        '1' * (child1[j] == '0')
+            for j in range(len(child2)):
+                # С вероятностью, определенной _probabilities_mutation[i], меняем значение гена на противоположное
+                if randint(0, 100) <= self._probabilities_mutation[i] * 100:
+                    child2[j] = '0' * (child2[j] == '1') + \
+                        '1' * (child2[j] == '0')
             # Добавляем потомка в список выбранной популяции
-            self._selected_population.append(child)
+            self._selected_population.append(child1)
+            self._selected_population.append(child2)
 
         return self._selected_population  # Возвращаем список выбранной популяции
+
+
+if __name__ == "__main__":
+    for i in range(100):
+        arr = []
+        for i in range(100):
+            arr.append(
+                (list(bin(randint(16, 31))[2:]), list(bin(randint(16, 32))[2:]), list(bin(randint(16, 31))[2:]), list(bin(randint(16, 31))[2:])))
+            x = ChangingMutator(arr, 0.6)
+            x.make_mutation()
