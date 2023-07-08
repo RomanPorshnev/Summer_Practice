721d87fd41249e91af85fbcb65413fdf0ae56842
evildre <90715546+evildre@users.noreply.github.com>
2023-07-08 23:12:46 +0300

    Update AbstractParentsSelector.py

--- Code/Facade/GeneticAlgorithm/AbstractParentsSelector/AbstractParentsSelector.py
+++ Code/Facade/GeneticAlgorithm/AbstractParentsSelector/AbstractParentsSelector.py
@@ -7,11 +7,15 @@ class AbstractParentsSelector(Protocol):
     _population: list
     _selected_parents: list
     _costs: list
+    _weights: list
+    _backpack_capacity: int
     # В конструктор передаётся список хромосом популяции
 
-    def __init__(self, population: list, costs: list):
+    def __init__(self, population: list, costs: list, weights: list, backpack_capacity: int):
         self._population = population
         self._costs = costs
+        self._weights = weights
+        self._backpack_capacity = backpack_capacity
         self._selected_parents = []
 
     '''
